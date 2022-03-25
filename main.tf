8d1bc557745f2b7a627fd6554fcf77abf3f4c1a3
satya2330 <satyadevops30@gmail.com>
2022-03-25 12:01:58 -0700

    credintials delete from local

--- main.tf
+++ main.tf
@@ -4,8 +4,4 @@
 provider "azurerm" {
   features {}
 
-  subscription_id = "a0ce6608-7c70-4f0e-9961-0331f61cb0f5"
-  client_id       = "e07f4383-2b59-4a96-9820-34d990e9eb41"
-  client_secret   = "E2I7Q~ojziKiPZ1IEFA.XsIXGlqcBf6pwhHUG"
-  tenant_id       = "e93eb566-717e-4ff2-8a16-543a2cd369d4"
 }
